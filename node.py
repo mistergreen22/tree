fea2333f38170730dd86ae19dd498a95578a5851
Pavlo Havrylov <greenpasha22@gmail.com>
2020-01-28 14:41:07 +0200

    add implementation

--- node.py
+++ node.py
@@ -15,22 +15,40 @@ more_vars = [
 
 
 class Node:
-    def __init__(self, child, value, parent=None):
-        self.parent = parent
-        self.child = child
-        self.value = value
-
+    def __init__(self, value, index, parent=None):
+        self._parent = parent
+        self._childred = []
+        self._value = value
+        self._index = index
 # Create Node() obj
 # format output f''
 # triple lvl of nesting using for
 # compare structure
 
-    def __str__(self):
-        return f''
-        pass
+    def add_child(self, child):
+        self._childred.append(child)
 
-    def __eq__(self, other):
-        pass
+
+
+
+
+    def __str__(self):
+        return f'{type(self._value)} -> {len(self._childred)}'
+
+    def uuid(self):
+        return f'{self._value if not len(self._childred) else ""}|{self._index}|{self._parent.uuid()}'
+
+
+
+def data_to_tree(data):
+    root = Node(data, 1)
+    for my_list, my_index in zip(data, range(len(data))):
+        var = Node(my_list, my_index, root)
+        root.add_child(var)
+        for sec_list, 
+    #.....
+
+    return root
 
 
 def important_method_for_someth():
